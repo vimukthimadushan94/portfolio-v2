3c0f788d659910ec6d5d087c0f0670180601ec05
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2024-09-24 23:12:01 +0300

    changes in profile section

--- layouts/components/Portfolio.js
+++ layouts/components/Portfolio.js
@@ -5,12 +5,7 @@ const Portfolio = ({ resume }) => {
     <section id="portfolio" className="portfolio section px-8">
       <div className="section-title container" data-aos="fade-up">
         <h2>Portfolio</h2>
-        <p>
-          Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-          aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-          quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-          fugiat sit in iste officiis commodi quidem hic quas.
-        </p>
+        <p>Here are a few projects I have developed independently.</p>
       </div>
       <div className="container">
         <div
@@ -45,158 +40,108 @@ const Portfolio = ({ resume }) => {
             </div>
 
             <div className="col-lg-4 col-md-6 portfolio-item isotope-item filter-product">
-              <Image
-                src="/images/webworks/pentium-pastal.jpg"
-                width={420}
-                height={500}
-                className="img-fluid"
-                alt=""
-              />
-              <div className="portfolio-info">
-                <h4>Product 1</h4>
-                <a
-                  href="https://pentium2000.lk/"
-                  target="__blank"
-                  title="App 1"
-                  data-gallery="portfolio-gallery-app"
-                  className="glightbox preview-link"
-                >
+              <a
+                href="https://pentium2000.lk/"
+                target="__blank"
+                title="App 1"
+                data-gallery="portfolio-gallery-app"
+                className="glightbox preview-link"
+              >
+                <Image
+                  src="/images/webworks/pentium-pastal.jpg"
+                  width={420}
+                  height={500}
+                  className="img-fluid"
+                  alt=""
+                />
+                <div className="portfolio-info">
                   <p>Pentium 2000 Stationery Company</p>
-
-                  <i className="bi bi-zoom-in"></i>
-                </a>
-                <a
-                  href="portfolio-details.html"
-                  title="More Details"
-                  className="details-link"
-                >
-                  <i className="bi bi-link-45deg"></i>
-                </a>
-              </div>
+                </div>
+              </a>
             </div>
 
             <div className="col-lg-4 col-md-6 portfolio-item isotope-item filter-branding">
-              <Image
-                src="/images/webworks/tuktuk.jpg"
-                width={420}
-                height={500}
-                className="img-fluid"
-                alt=""
-              />
-              <div className="portfolio-info">
-                <h4>Branding 1</h4>
-                <a
-                  href="http://tuktukceylonride.com/"
-                  target="__blank"
-                  title="App 1"
-                  data-gallery="portfolio-gallery-app"
-                  className="glightbox preview-link"
-                >
+              <a
+                href="http://tuktukceylonride.com/"
+                target="__blank"
+                title="App 1"
+                data-gallery="portfolio-gallery-app"
+                className="glightbox preview-link"
+              >
+                <Image
+                  src="/images/webworks/tuktuk.jpg"
+                  width={420}
+                  height={500}
+                  className="img-fluid"
+                  alt=""
+                />
+                <div className="portfolio-info">
                   <p>Tuk Tuk CeylonRide</p>
-
-                  <i className="bi bi-zoom-in"></i>
-                </a>
-                <a
-                  href="portfolio-details.html"
-                  title="More Details"
-                  className="details-link"
-                >
-                  <i className="bi bi-link-45deg"></i>
-                </a>
-              </div>
+                </div>
+              </a>
             </div>
 
             <div className="col-lg-4 col-md-6 portfolio-item isotope-item filter-app">
-              <Image
-                src="/images/webworks/heslaa.jpg"
-                width={420}
-                height={500}
-                className="img-fluid"
-                alt=""
-              />
-              <div className="portfolio-info">
-                <h4>App 2</h4>
-                <a
-                  href="https://heslaatours.com/"
-                  target="__blank"
-                  title="App 1"
-                  data-gallery="portfolio-gallery-app"
-                  className="glightbox preview-link"
-                >
+              <a
+                href="https://heslaatours.com/"
+                target="__blank"
+                title="App 1"
+                data-gallery="portfolio-gallery-app"
+                className="glightbox preview-link"
+              >
+                <Image
+                  src="/images/webworks/heslaa.jpg"
+                  width={420}
+                  height={500}
+                  className="img-fluid"
+                  alt=""
+                />
+                <div className="portfolio-info">
                   <p>Heslaa Tours</p>
-
-                  <i className="bi bi-zoom-in"></i>
-                </a>
-                <a
-                  href="portfolio-details.html"
-                  title="More Details"
-                  className="details-link"
-                >
-                  <i className="bi bi-link-45deg"></i>
-                </a>
-              </div>
+                </div>
+              </a>
             </div>
 
             <div className="col-lg-4 col-md-6 portfolio-item isotope-item filter-product">
-              <Image
-                src="/images/webworks/baranas.jpg"
-                width={420}
-                height={500}
-                className="img-fluid"
-                alt=""
-              />
-              <div className="portfolio-info">
-                <h4>Product 2</h4>
-                <a
-                  href="https://baranassrilanka.com/"
-                  target="__blank"
-                  title="App 1"
-                  data-gallery="portfolio-gallery-app"
-                  className="glightbox preview-link"
-                >
+              <a
+                href="https://baranassrilanka.com/"
+                target="__blank"
+                title="App 1"
+                data-gallery="portfolio-gallery-app"
+                className="glightbox preview-link"
+              >
+                <Image
+                  src="/images/webworks/baranas.jpg"
+                  width={420}
+                  height={500}
+                  className="img-fluid"
+                  alt=""
+                />
+                <div className="portfolio-info">
                   <p>Baranas Sri Lanka</p>
-
-                  <i className="bi bi-zoom-in"></i>
-                </a>
-                <a
-                  href="portfolio-details.html"
-                  title="More Details"
-                  className="details-link"
-                >
-                  <i className="bi bi-link-45deg"></i>
-                </a>
-              </div>
+                </div>
+              </a>
             </div>
 
             <div className="col-lg-4 col-md-6 portfolio-item isotope-item filter-branding">
-              <Image
-                src="/images/webworks/enrich-website.jpg"
-                width={420}
-                height={500}
-                className="img-fluid"
-                alt=""
-              />
-              <div className="portfolio-info">
-                <h4>Branding 2</h4>
-                <a
-                  href="https://www.enrichtea.com/"
-                  target="__blank"
-                  title="App 1"
-                  data-gallery="portfolio-gallery-app"
-                  className="glightbox preview-link"
-                >
+              <a
+                href="https://www.enrichtea.com/"
+                target="__blank"
+                title="App 1"
+                data-gallery="portfolio-gallery-app"
+                className="glightbox preview-link"
+              >
+                <Image
+                  src="/images/webworks/enrich-website.jpg"
+                  width={420}
+                  height={500}
+                  className="img-fluid"
+                  alt=""
+                />
+                <div className="portfolio-info">
                   <p>Enrich tea and food exports</p>
-
-                  <i className="bi bi-zoom-in"></i>
-                </a>
-                <a
-                  href="portfolio-details.html"
-                  title="More Details"
-                  className="details-link"
-                >
-                  <i className="bi bi-link-45deg"></i>
-                </a>
-              </div>
+                </div>
+              </a>
             </div>
           </div>
         </div>
